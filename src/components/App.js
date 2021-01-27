01ce1d1fbc9d0dab5d2cd53aa32e6bfc1ccebc64
Daniel Gamboa <danielgamboa10@gmail.com>
2021-01-27 15:51:59 -0600

    Set up basic structure for App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from './Form';
+
+// - [ ] Name
+// - [ ] Email
+// - [ ] Password
+// - [ ] Terms of Service (checkbox)
+// - [ ] A Submit button to send our form data to the server.
+
+// (1) Initial States:
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  termsOfService: false
+};
+
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+  termsOfService: ''
+};
+
+const initialDisabled = true;
+const initialUsers = [];
 
 export default function App() {
+  // (2) Application State Hooks:
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+  const [users, setUsers] = useState(initialUsers);
+
+  // () Helper Functions:
+
   return (
+    // (3) Basic JSX Structure for App:
     <div className="App">
-      hello world
+      <header><h1>User Onboarding App</h1></header>
+
+      <Form />
+
+      {
+        users.map(user => {
+          return <User key={user.id} details={user}/>
+        })
+      }
     </div>
   );
 }
